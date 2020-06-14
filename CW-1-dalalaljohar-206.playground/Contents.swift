019140c1a17daff5f7a861d0897296c440c29d02
daallaa <dalal.alj003@GMAIL.COM>
2020-06-14 23:20:42 +0300

    Done

---
+++ CW-1-dalalaljohar-206.playground/Contents.swift
@@ -0,0 +1,35 @@
+import UIKit
+//part1
+let name = "Dalal"
+var hoppy1 = "reading"
+var hoppy2 = " drawing"
+var age = 17
+var height = 166
+
+print ("my name is: ", name, ", and my age is \(age) , and my height is : \(height) , my hobbies are :\(hoppy1), \(hoppy2)")
+
+//part2
+var firendName = "fatma"
+var firendHeight = 155
+var firendAge = 16
+var firendhoppy1 = "drawing"
+var firendhoppy2 = "swimming"
+
+//comparing
+name == firendName
+name != firendName
+
+
+hoppy1 == firendhoppy1
+hoppy2 != firendhoppy2
+
+age > firendAge
+
+height > firendHeight
+
+ let BestFriends =
+hoppy1 != firendhoppy1  &&
+hoppy1 != firendhoppy2  &&
+hoppy2 == firendhoppy1  &&
+hoppy2 != firendhoppy2  &&
+name == firendName
